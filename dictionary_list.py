317f7916a7700ec30e468d209944dc8da0252ef7
Rodolphe Beloncle <69888160+rodicody@users.noreply.github.com>
2021-08-20 07:02:26 +0200

    Update dictionary_list.py

--- dictionary_list.py
+++ dictionary_list.py
@@ -77,8 +77,13 @@ travel_log = {
 #Nesting Dictionary in a Dictionary
 
 travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+  "France": 
+  {"cities_visited": ["Paris", "Lille", "Dijon"], 
+   "total_visits": 12},
+  
+  "Germany": 
+  {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+   "total_visits": 5},
 }
 
 #Nesting Dictionaries in Lists
