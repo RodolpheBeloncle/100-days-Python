b826c86179fcda50547c5c98a15a15237aa5dd70
Rodolphe Beloncle <69888160+rodicody@users.noreply.github.com>
2021-08-21 13:40:13 +0200

    Update nested_function_dict.py

--- nested_function_dict.py
+++ nested_function_dict.py
@@ -26,7 +26,7 @@ operations = {
 
 num1 = int(input("What's the first number ? : "))
 
-num2 = int(input("What's the second number ? : "))
+
 
 for symbol in operations:
   print(symbol)
@@ -34,6 +34,21 @@ for symbol in operations:
 
 operation_symbol = input("Pick an operation from the line above: ")
 
+num2 = int(input("What's the second number ? : "))
+
 operation_process = operations[operation_symbol](num1,num2)
 
+# solution N°2: calculation_function =  operations[operation_symbol]
+
+# => answer = calculation_function(num1,num2)
+# => calculate a result from the preview's result 
+
 print(f"{num1}{operation_symbol}{num2} = {operation_process}")
+
+operation_symbol = input("pick another operation: ")
+num3 = int(input("what's the next number?: "))
+
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1,num2),num3)
+print(f"{num1}{operation_symbol}{num2} = {second_answer}")
+
